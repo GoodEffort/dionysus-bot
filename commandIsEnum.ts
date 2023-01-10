94cb3456354b73c01c504eaf1efc1e87e94f64b2
Luke Hovarter <lukehsurvey@gmail.com>
2023-01-10 17:23:09 -0500

    added a way to check if a string is in an enum

---
+++ commandIsEnum.ts
@@ -0,0 +1,9 @@
+type StandardEnum = {
+    [id: string]: string;
+};
+export function commandIsEnum<
+    T extends StandardEnum,
+    K extends keyof T>(enumerator: T, commandName: string): commandName is T[K] {
+    return (Object.values(enumerator) as string[]).includes(commandName);
+}
+export default commandIsEnum;
