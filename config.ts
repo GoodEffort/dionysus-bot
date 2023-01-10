0c08bb0756da78bd9b03f130eebae7894f57f242
Luke Hovarter <lukehsurvey@gmail.com>
2023-01-10 17:36:01 -0500

    typescript doesn't allow josn imports by default :)

---
+++ config.ts
@@ -0,0 +1,11 @@
+export const Minecraft = {
+    "port": 25565,
+    "screenSession": "minecraft",
+    "start": "/home/steam/minecraft/scripts/start-screen-session.sh -t",
+    "getPlayers": "./scripts/minecraft/bot-get-number-of-players.sh",
+    "checkStatus": "/home/steam/minecraft/scripts/check-minecraft-server.sh"
+};
+
+export const Valheim = {};
+export const Satisfactory = {};
+export const aFactorio = {};
