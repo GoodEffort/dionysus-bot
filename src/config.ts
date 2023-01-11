7427938d529486bf624379c161486e35594d148b
Luke Hovarter <lukehsurvey@gmail.com>
2023-01-11 15:02:11 -0500

    changed configs to enums

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,29 @@
-export const Minecraft = {
-    "port": 25565,
-    "screenSession": "minecraft",
-    "start": "/home/steam/minecraft/scripts/start-screen-session.sh -t",
-    "getPlayers": "./scripts/minecraft/bot-get-number-of-players.sh",
-    "checkStatus": "/home/steam/minecraft/scripts/check-minecraft-server.sh"
+export enum BotConfig {
+    // bot install directory
+    installDirectory = "~/dionysus-bot/",
+
+    // bot script directory (in bot install directory)
+    scriptDirectory = "scripts/",
+}
+
+export enum Minecraft {
+    // script vars
+    port = 25565,
+    screenSession = "minecraft",
+
+    // minecraft server directory
+    installDirectory = "~/minecraft/",
+
+    // script directory (in bot script directory)
+    scriptDirectory = "minecraft/",
+
+    // script names and flags
+    start = "start-screen-session.sh -t",
+    getPlayers = "bot-get-number-of-players.sh",
+    checkStatus = "check-minecraft-server.sh",
+    stop = "send-command.sh -c stop",
 };
 
-export const Valheim = {};
-export const Satisfactory = {};
-export const aFactorio = {};
+export enum Valheim {};
+export enum Satisfactory {};
+export enum Factorio {};
