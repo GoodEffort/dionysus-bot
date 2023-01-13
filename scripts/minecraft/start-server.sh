86c3e44868cbc134f817d4f64d55e9747569683c
Luke Hovarter <lukehsurvey@gmail.com>
2023-01-13 01:24:39 -0500

    anotha one

--- scripts/minecraft/start-server.sh
+++ scripts/minecraft/start-server.sh
@@ -17,18 +17,18 @@ while getopts 'm:s:j:c:t' flag; do
 done
 
 servercommand="java -Xmx$mem -Xms$mem \
-        -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
-        -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
-        -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
-        -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 \
-        -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
-        -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
-        -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 \
-        -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
-        -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
-        -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
-        -jar $jar \
-        /"
+  -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+  -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
+  -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
+  -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 \
+  -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
+  -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
+  -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 \
+  -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
+  -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
+  -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
+  -jar $jar \
+  /"
 
 #echo $servercommand
 echo "Starting Minecraft server with $mem of memory using the $jar as the server jar"
@@ -37,20 +37,18 @@ touch $rootdir/activity/$screensession
 echo "s:$(date +%s):" >> $rootdir/activity/$screensession
 
 if [ $timeout = true ] ; then
-  ./edit-cron-job.sh
+  $rootdir/edit-cron-job.sh
 fi
 
 cd $minecraftInstallationDir
 
 until $servercommand; do
-        echo "Minecraft server crashed with exit code $?. Restarting in 10 seconds" >&2
-        sleep 10
+  echo "Minecraft server crashed with exit code $?. Restarting in 10 seconds" >&2
+  sleep 10
 done
-
-cd $rootdir
 
 echo "Server closed successfully!"
 
 if [ $timeout = true ] ; then
-  ./edit-cron-job.sh -r
+  $rootdir/edit-cron-job.sh -r
 fi
